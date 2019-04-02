b50f1f422d50499e578fda750b00fc5192c8768d
Jase Owens <jaseowens197@gmail.com>
2019-04-02 00:20:36 -0500

    slight fix for cors, possibly

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
     next();
 });
 
